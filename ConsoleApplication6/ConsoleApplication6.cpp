79fe702235fd37c0c4fb80317b47417173e4f0c7
Кира Бузмакова <kirabuzmakova91199@gmail.com>
2025-01-15 18:32:58 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n = get_input(); // Получаем корректный ввод
+    string result = sum_of_factorials(n);
+    cout << "Сумма факториалов от 1 до " << n << " равна: " << result << endl;
+    return 0;
+}
