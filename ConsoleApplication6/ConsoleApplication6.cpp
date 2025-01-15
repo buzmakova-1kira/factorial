28a13d9646cd0a713a9e9501d95035ed576b68b5
Кира Бузмакова <kirabuzmakova91199@gmail.com>
2025-01-15 18:35:26 +0500

    корректный ввод

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -2,6 +2,24 @@
 #include <string>
 using namespace std;
 
+// Функция для получения корректного ввода от пользователя
+int get_input() {
+    int n;
+    while (true) {
+        cout << "Введите целое число n (n > 10): ";
+        cin >> n;
+
+        if (cin.fail() || n <= 10) {
+            cin.clear(); // сбросить состояние потока
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // игнорировать некорректный ввод
+            cout << "Ошибка: введите целое число больше 10." << endl;
+        }
+        else {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // очистить буфер ввода
+            return n;
+        }
+    }
+}
 int main() {
     setlocale(LC_ALL, "Russian");
     int n = get_input(); // Получаем корректный ввод
